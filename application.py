03fb02060b8b9fd186d5607f2c1ff6f2f2c1e5ea
Stephen <stephen.gnoza@gmail.com>
2019-02-26 07:37:08 -0500

    Set up basic application.py file

---
+++ application.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+#from database_setup import Base, Restaurant, MenuItem
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def HelloWorld():
+    return "Hello World"
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='localhost', port=5000)
