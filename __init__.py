2e43e66d4be9a58d5f9e809e0b4328c9723cf0fa
Stephen <stephen.gnoza@gmail.com>
2019-04-03 07:43:41 -0400

    Update path to client_secrets.json

--- __init__.py
+++ __init__.py
@@ -18,7 +18,7 @@ import requests
 app = Flask(__name__)
 
 CLIENT_ID = json.loads(
-    open('client_secrets.json', 'r').read())['web']['client_id']
+    open('/var/www/ultracatalogue/client_secrets.json', 'r').read())['web']['client_id']
 APPLICATION_NAME = "Ultra Marathon Catalogue"
 
 # Connect to Database and create database session
